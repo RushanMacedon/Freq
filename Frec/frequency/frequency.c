93ca849cc79cf822ba582e0748d3b73d24c22f3b
nikazzz <macedon622@gmail.com>
2021-03-16 15:47:23 +0300

    ad

--- Frec/frequency/frequency.c
+++ Frec/frequency/frequency.c
@@ -28,7 +28,7 @@ void freq_init(freq_t *p_t)
 {
    nrf_gpio_cfg_output(p_t->pin_number);
      nrf_drv_timer_config_t timer_cfgd = {                                                                               \
-    .frequency          = (nrf_timer_frequency_t)TIMER_DEFAULT_CONFIG_FREQUENCY,\
+    .frequency          = (nrf_timer_frequency_t)NRF_TIMER_FREQ_500kHz,\
     .mode               = (nrf_timer_mode_t)TIMER_DEFAULT_CONFIG_MODE,          \
     .bit_width          = (nrf_timer_bit_width_t)TIMER_DEFAULT_CONFIG_BIT_WIDTH,\
     .interrupt_priority = TIMER_DEFAULT_CONFIG_IRQ_PRIORITY,                    \
@@ -51,8 +51,16 @@ void freq_timer_start(uint32_t frequency)
          &TIMER_FREQ, NRF_TIMER_CC_CHANNEL0, time_ticks, NRF_TIMER_SHORT_COMPARE0_CLEAR_MASK, true);
 
     nrf_drv_timer_enable(&TIMER_FREQ);
- }     
- 
+ }   
+void freq_timer_change(uint32_t frequency)
+{
+    uint32_t time_us = 1000000/(2*frequency); 
+    uint32_t time_ticks;
+    time_ticks = nrf_drv_timer_us_to_ticks(&TIMER_FREQ, time_us);
+
+    nrf_drv_timer_extended_compare(
+         &TIMER_FREQ, NRF_TIMER_CC_CHANNEL0, time_ticks, NRF_TIMER_SHORT_COMPARE0_CLEAR_MASK, true);
+}    
   void freq_timer_stop(void)
  {
      nrf_drv_timer_disable(&TIMER_FREQ);
